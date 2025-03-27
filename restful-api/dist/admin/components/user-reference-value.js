3c21947ace8e5f65ef5a119b92733172bdf6a152
niu <123456@qq.com>
2025-03-27 21:40:07 +0800

    Add submodules

--- restful-api/dist/admin/components/user-reference-value.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-const UserReferenceValue = ({ inviter }) => {
-    if (!inviter)
-        return null;
-    return (React.createElement("span", null, inviter.phone));
-};
-export default UserReferenceValue;
